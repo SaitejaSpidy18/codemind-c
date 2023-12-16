253038aff0c8fba54056121acba7ad4a5c3cb464
SaitejaSpidy18 <147791952+SaitejaSpidy18@users.noreply.github.com>
2023-12-16 09:58:50 +0530

    #include<stdio.h> int main() { 	int n,m,i; 	scanf("%d",&n); 	int a[n]; 	for( i=0;i<n;i++) 	{ 		scanf("%d",&a[i]); 	} 	m=a[0]; 	for(i=1;i<n;i++) 	{ 		if(m>a[i]) 		{ 			m=a[i]; 		} 	} 	printf("%d",m); }

---
+++ Minimum_element_of_an_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n,m,i;
+	scanf("%d",&n);
+	int a[n];
+	for( i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	m=a[0];
+	for(i=1;i<n;i++)
+	{
+		if(m>a[i])
+		{
+			m=a[i];
+		}
+	}
+	printf("%d",m);
+}
