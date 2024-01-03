d363a8b5c9c12e5a775ef93f8198067efa9ed933
SaitejaSpidy18 <147791952+SaitejaSpidy18@users.noreply.github.com>
2024-01-03 18:21:57 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=n;1<=i;i--)     {         for(j=i;1<=j;j--)         {             printf("* ",i);         }         printf(" ");     }
    }

---
+++ Basic_Pattern_3.1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    scanf("%d",&n);
+    for(i=n;1<=i;i--)
+    {
+        for(j=i;1<=j;j--)
+        {
+            printf("* ",i);
+        }
+        printf("
+");
+    }
+    
+}
